a8ad55e873d16d9889d69e528ca53127bc9235b1
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-13 16:16:49 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onChangeBackground);
+
+function onChangeBackground() {
+    
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
